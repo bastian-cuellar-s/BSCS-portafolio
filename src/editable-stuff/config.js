f853cabaecda937cf24135fbfc37a74c67752b77
Bastián SCS <bastian.cuellar.s@gmail.com>
2023-07-21 23:02:28 -0400

    Primer commit, cambios personales al template

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -6,32 +6,32 @@ const navBar = {
 // Main Body SECTION
 const mainBody = {
   gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
-  firstName: "Hashir",
+  firstName: "Bastián",
   middleName: "",
-  lastName: "Shoaib",
-  message: " Passionate about changing the world with technology. ",
+  lastName: "Cuellar Salinas",
+  message: " Passionate about learning and leveraging technology to drive positive change and make a difference in the world. ",
   icons: [
     {
       image: "fa-github",
-      url: "https://github.com/hashirshoaeb",
+      url: "https://github.com/bastian-cuellar-s",
     },
     {
       image: "fa-facebook",
-      url: "https://www.facebook.com/hashirshoaeb",
+      url: "https://www.facebook.com/profile.php?id=100094333365773",
     },
     {
       image: "fa-instagram",
-      url: "https://www.instagram.com/hashirshoaeb/",
+      url: "https://www.instagram.com/bastian.cuellar.s/",
     },
     {
       image: "fa-linkedin",
-      url: "https://www.linkedin.com/in/hashirshoaeb/",
+      url: "https://www.linkedin.com/in/bastian-cuellar-salinas/",
     },
     {
       image: "fa-twitter",
-      url: "https://www.twitter.com/hashirshoaeb/",
+      url: "https://twitter.com/bastian_scs",
     },
-  ],
+  ],  
 };
 
 // ABOUT SECTION
@@ -54,7 +54,7 @@ const about = {
   imageLink: require("../editable-stuff/hashirshoaeb.png"),
   imageSize: 375,
   message:
-    "My name is Hashir Shoaib. I’m a graduate of 2020 from National University of Sciences and Technology at Islamabad with a degree in Computer Engineering. I'm most passionate about giving back to the community, and my goal is to pursue this passion within the field of software engineering. In my free time I like working on open source projects.",
+    "My name is Bastián Cuellar. I hold a degree in Computer Engineering from the Pontifical Catholic University of Valparaiso. As a lifelong learner, I am passionate about exploring and growing in the field of software engineering. I am eager to contribute to the community and make a positive impact through my work. Currently, I am focused on expanding my knowledge and skills in various programming languages and technologies. I actively engage in personal projects and open-source initiatives to further enhance my learning. I am excited to continue my journey in software engineering, continuously expanding my expertise and embracing new challenges.",
   resume: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
 };
 
@@ -66,7 +66,7 @@ const about = {
 const repos = {
   show: true,
   heading: "Recent Projects",
-  gitHubUsername: "hashirshoaeb", //i.e."johnDoe12Gh"
+  gitHubUsername: "bastian-cuellar-s", //i.e."johnDoe12Gh"
   reposLength: 4,
   specificRepos: [],
 };
@@ -100,24 +100,24 @@ const skills = {
   show: true,
   heading: "Skills",
   hardSkills: [
-    { name: "Python", value: 90 },
-    { name: "SQL", value: 75 },
-    { name: "Data Structures", value: 85 },
-    { name: "C/C++", value: 65 },
-    { name: "JavaScript", value: 90 },
+    { name: "JavaScript", value: 80 },
+    { name: "VueJs", value: 75 },
     { name: "React", value: 65 },
+    { name: "Laravel", value: 80 },
+    { name: "PHP", value: 75 },
+    { name: "SQL", value: 70 },
+    { name: "C/C++", value: 55 },
     { name: "HTML/CSS", value: 55 },
-    { name: "C#", value: 80 },
   ],
   softSkills: [
+    { name: "Empathy", value: 90 },
     { name: "Goal-Oriented", value: 80 },
     { name: "Collaboration", value: 90 },
-    { name: "Positivity", value: 75 },
-    { name: "Adaptability", value: 85 },
+    { name: "Positivity", value: 85 },
+    { name: "Adaptability", value: 80 },
     { name: "Problem Solving", value: 75 },
-    { name: "Empathy", value: 90 },
-    { name: "Organization", value: 70 },
-    { name: "Creativity", value: 90 },
+    { name: "Creativity", value: 70 },
+    { name: "Organization", value: 65 },
   ],
 };
 
@@ -126,8 +126,8 @@ const getInTouch = {
   show: true,
   heading: "Get In Touch",
   message:
-    "I'm currently looking for full-time Software Engineering or Machine Learning opportunities! If you know of any positions available, if you have any questions, or if you just want to say hi, please feel free to email me at",
-  email: "hashirshoaeb@gmail.com",
+    "I'm currently looking for full-time Software Engineering! If you know of any positions available, if you have any questions, or if you just want to say hi, please feel free to email me at",
+  email: "bastian.cuellar.s@gmail.com",
 };
 
 const experiences = {
@@ -135,14 +135,14 @@ const experiences = {
   heading: "Experiences",
   data: [
     {
-      role: 'Software Engineer',// Here Add Company Name
+      role: 'Independent FullStack Developer',// Here Add Company Name
       companylogo: require('../assets/img/dell.png'),
-      date: 'June 2018 – Present',
+      date: '2021 – Present',
     },
     {
-      role: 'Front-End Developer',
+      role: 'Consultant and IT Advisor',
       companylogo: require('../assets/img/boeing.png'),
-      date: 'May 2017 – May 2018',
+      date: 'July 2020 – June 2022',
     },
   ]
 }
